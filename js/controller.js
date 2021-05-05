2d1802c8e972ff19bc494fea4b7a0014ae831e77
aleksandrstepin <gribanov32rus777@gmail.com>
2021-05-05 19:09:09 +0300

    ...

---
+++ js/controller.js
@@ -0,0 +1,87 @@
+var Controller = function (){
+
+    const model = new Model();
+    const view = new View(model, onClickCell);
+
+    function onClickCell(X, Y, isChecker){
+        if (isChecker){
+            model.x = X;
+            model.y = Y;
+            view.render();
+        } else {
+            if (model.x !== null && model.y !== null){
+                //Черные фигуры
+                console.log(model.y, model.x);
+                console.log(Y, X);
+                if (model.cells[model.y][model.x] === 1){
+                    if (model.y !== 7) {
+                        if (((model.x - 1) === X && (model.y + 1) === Y) || ((model.x + 1) === X && (model.y + 1) === Y)) {
+                            model.cells[Y][X] = model.cells[model.y][model.x];
+                            model.cells[model.y][model.x] = 0;
+
+
+                            view.render();
+                            clearSelected();
+                        } else if ((model.cells[model.y + 1][model.x + 1] === 2) && ((model.x + 2) === X && (model.y + 2) === Y)) {
+                            console.log("white");
+
+                            model.cells[Y][X] = model.cells[model.y][model.x];
+                            model.cells[model.y + 1][model.x + 1] = 0;
+                            model.cells[model.y][model.x] = 0;
+
+                            view.render();
+                            clearSelected();
+                        } else if ((model.cells[model.y + 1][model.x - 1] === 2) && ((model.x - 2) === X && (model.y + 2) === Y)) {
+                            console.log("white");
+
+                            model.cells[Y][X] = model.cells[model.y][model.x];
+                            model.cells[model.y + 1][model.x - 1] = 0;
+                            model.cells[model.y][model.x] = 0;
+
+
+                            view.render();
+                            clearSelected();
+                        }
+                    } else {
+                        alert("Black Win!!!")
+                    }
+                } else {
+                    //Белые фигуры
+                    if (model.y !== 1) {
+                        if (((model.x + 1) === X && (model.y - 1) === Y) || ((model.x - 1) === X && (model.y - 1) === Y)) {
+                            model.cells[Y][X] = model.cells[model.y][model.x];
+                            model.cells[model.y][model.x] = 0;
+
+
+                            view.render();
+                            clearSelected();
+                        } else if ((model.cells[model.y - 1][model.x + 1] === 1) && ((model.x + 2) === X && (model.y - 2) === Y)) {
+                            console.log("black");
+
+                            model.cells[Y][X] = model.cells[model.y][model.x];
+                            model.cells[model.y - 1][model.x + 1] = 0;
+                            model.cells[model.y][model.x] = 0;
+
+                            view.render();
+                            clearSelected();
+                        } else if ((model.cells[model.y - 1][model.x - 1] === 1) && ((model.x - 2) === X && (model.y - 2) === Y)) {
+                            console.log("black");
+
+                            model.cells[Y][X] = model.cells[model.y][model.x];
+                            model.cells[model.y - 1][model.x - 1] = 0;
+                            model.cells[model.y][model.x] = 0;
+
+
+                            view.render();
+                            clearSelected();
+                        }
+                    } else {
+                        alert("White Win!!!");
+                    }
+                }
+            }
+        }
+    }
+
+    view.render();
+}
---
+++ js/view.js
@@ -0,0 +1,40 @@
+var View = function (model, onclickCell) {
+
+    this._model = model;
+    this._onclick = function (x, y, z) {
+        onclickCell(x, y ,z);
+    }
+
+}
+
+View.prototype.render = function () {
+    const board = document.getElementById("board");
+    board.innerHTML = "";
+
+    this._model.cells.forEach((raw, outerIndex) => {
+        raw.forEach((cell, innerIndex) => {
+            const id = outerIndex * raw.length + innerIndex;
+            // board.innerHTML += `<div id="${id}"
+            //     class="board-tile ${(innerIndex + outerIndex) % 2 === 0 ? "bt-white" : "bt-black"}">
+            //     ${cell === 1 ? "<div class='black-checker'></div>" : cell === 2 ? "<div class='white-checker'></div>" : ""} </div>`
+            // // const elem = document.getElementById(id);
+
+            const element = document.createElement("div");
+            element.innerHTML = `
+                ${cell === 1 ? "<div class='black-checker'></div>" : cell === 2 ? "<div class='white-checker'></div>" : ""}`
+
+            element.id = id;
+            element.classList.add(`board-tile`, (innerIndex + outerIndex) % 2 === 0 ? "bt-white" : "bt-black");
+
+            element.onclick = () => this._onclick(innerIndex, outerIndex, cell !== 0);
+
+            if (this._model.x === innerIndex && this._model.y === outerIndex){
+                element.classList.add(`selected`);
+            }
+            board.appendChild(element);
+        });
+    });
+
+
+}
+
